7a0328d0c3f8f3c540a2f3966cc4f38934aca1df
al3nka <kiychenkoak@gmail.com>
2024-02-07 13:36:53 +0100

    Change the tex draft processing

--- src/main/jinja_pdf_utils.py
+++ src/main/jinja_pdf_utils.py
@@ -1,15 +1,11 @@
-import os
-
 from jinja2 import Environment, FileSystemLoader, meta
 
-tex_draft_dir = 'media/'
-jinja_environment = Environment(loader=FileSystemLoader(tex_draft_dir))
+media_dir = 'media/'
+jinja_environment = Environment(loader=FileSystemLoader(media_dir))
 
 
-def get_tex_draft_variable_names(tex_draft_filename: str) -> tuple:
-    tex_draft_location = tex_draft_dir + tex_draft_filename
-    with open(tex_draft_location, 'r') as file:
-        tex_draft_data = file.read()
+def get_tex_draft_variable_names(tex_draft_file) -> tuple:
+    tex_draft_data = tex_draft_file.open('r').read()
     parsed_data = jinja_environment.parse(tex_draft_data)
     variables = tuple(meta.find_undeclared_variables(parsed_data))
     return variables
