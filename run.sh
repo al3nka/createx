42a5f4bc52d5e05c64c95048d2314e597147dd0c
al3nka <kiychenkoak@gmail.com>
2024-03-01 13:31:27 +0100

    Remove setting default values for django superuser

--- run.sh
+++ run.sh
@@ -5,12 +5,6 @@ set -e
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
-if [ -z "$DJANGO_SUPERUSER_USERNAME" ]; then
-  export DJANGO_SUPERUSER_USERNAME=admin
-fi
-if [ -z "$DJANGO_SUPERUSER_EMAIL" ]; then
-  export DJANGO_SUPERUSER_EMAIL=admin@email.com
-fi
 if [ "$CREATE_SUPERUSER" == "true" ]; then
   python manage.py createsuperuser --noinput
 fi
