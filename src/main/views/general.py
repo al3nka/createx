51b2ddc54019a5f96ed1a2d7feb5dca4696ecdbd
al3nka <kiychenkoak@gmail.com>
2024-03-06 18:25:58 +0100

    Add user welcome page

--- src/main/views/general.py
+++ src/main/views/general.py
@@ -1,12 +1,16 @@
 from django.views.generic import TemplateView
 import logging
 
+from main.models import TexDraft
+
 logger = logging.getLogger(__name__)
 
 
 class LandingView(TemplateView):
-    template_name = 'index.html'
 
+    @property
+    def user(self):
+        return self.request.user
     def get(self, request, *args, **kwargs):
         logger.info('Landing page visited', extra={
             'user': str(request.user),
@@ -14,3 +18,19 @@ class LandingView(TemplateView):
             'user_agent': request.META.get('HTTP_USER_AGENT')
         })
         return super().get(request, *args, **kwargs)
+
+    def get_template_names(self):
+        if self.request.user.is_authenticated:
+            return 'user_welcome.html'
+        else:
+            return 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        if self.user.is_authenticated:
+            context['tex_drafts'] = self.get_user_tex_drafts()
+        return super().get_context_data() | context
+
+    def get_user_tex_drafts(self):
+        return TexDraft.objects.filter(owner=self.user)
+
