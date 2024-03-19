336324345ce0c8e21644bde2e5e83bebe6c951c8
al3nka <kiychenkoak@gmail.com>
2024-03-19 13:21:14 +0100

    Set correct directory for staticfiles

--- src/createx/settings.py
+++ src/createx/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.environ.get('STATIC_ROOT')
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key draft_field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
